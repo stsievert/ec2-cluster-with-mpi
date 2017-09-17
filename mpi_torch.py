6bea84e362a2edfdc72186c99a901287fdb0159d
Scott Sievert <scott@stsievert.com>
2017-09-17 14:09:02 -0500

    DOC: i{send, recv}

--- mpi_torch.py
+++ mpi_torch.py
@@ -6,6 +6,25 @@ comm = MPI.COMM_WORLD
 
 
 def isend(data, tag=None, dest=None, send=comm.Isend):
+    """
+    Parameters
+    ----------
+    data : np.ndarray, torch.Tensor
+        Data to send
+    tag : int
+        Message identifier
+    dest : int
+        Machine to send to
+    send : callable, str
+        The function to call (from mpi4py.MPI.COMM_WORLD)
+
+    Returns
+    -------
+    The return value of `send`
+
+    """
+    if isinstance(send, str):
+        send = getattr(comm, send)
     if not isinstance(tag, int):
         raise ValueError('Pass an integer tag (mpi4py supports ints only)')
     assert dest is not None, "Specicy a destination"
@@ -14,10 +33,29 @@ def isend(data, tag=None, dest=None, send=comm.Isend):
     if isinstance(data, torch.FloatTensor):
         data = data.numpy()
 
-    send(data, dest=dest, tag=tag)
+    return send(data, dest=dest, tag=tag)
 
 
 def irecv(data, tag=None, source=None, recv=comm.Irecv):
+    """
+    Parameters
+    ----------
+    data : np.ndarray, torch.Tensor
+        Data to send
+    tag : int
+        Message identifier
+    source : int
+        Machine to receive from
+    recv : callable, str
+        The function to call (from mpi4py.MPI.COMM_WORLD)
+
+    Returns
+    -------
+    The return value of `recv`
+
+    """
+    if isinstance(recv, str):
+        recv = getattr(comm, recv)
     assert tag is not None, "Specify a tag"
     assert source is not None, "Specify a source"
     return recv(data, source=source, tag=tag)
