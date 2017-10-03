24a97dfbb4c50efb7fc2ce62fb2d98bc7d0a32cb
Scott Sievert <sievert.scott@icloud.com>
2017-10-03 14:46:04 -0500

    MAINT: folder moving

--- scp.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-DNSs = ['ec2-54-190-37-5.us-west-2.compute.amazonaws.com',
-        'ec2-34-213-5-153.us-west-2.compute.amazonaws.com']
-
-key_file = '/Users/scott/Work/Developer/AWS/scott-key-dim.pem'
-for dns in DNSs:
-    cmd = 'scp -i {key_file} -r setup_scripts ec2-user@{dns}:~'
-    subprocess.call(cmd.format(dns=dns, key_file=key_file).split(' '))
--- torch_tcp.py
+++
@@ -1,6 +0,0 @@
-
-import torch
-import numpy as np
-import torch.distributed as dist
-
-dist.init_process_group()
