d88896a024671f265a47b95d8dbef1a0a3dbcd7a
Scott Sievert <sievert.scott@icloud.com>
2017-12-07 15:45:59 -0600

    MAINT: install dask

--- setup_scripts/install_deps.sh
+++ setup_scripts/install_deps.sh
@@ -7,3 +7,4 @@ sudo yum install -y openssh-server openssh-clients
 
 # `sudo pip != ancaond pip`!
 /home/ec2-user/anaconda3/bin/pip install mpi4py
+/home/ec2-user/anaconda3/bin/pip install dask distributed --upgrade
